c1a8dc2e4a1711042c5c3fbbec3ad6270279ac2b
Harold <galangharold56@gmail.com>
2023-12-05 11:13:07 +0800

    day 2 changes

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+var loader = document.getElementById("preloader");
+        window.addEventListener("load", function() {
+            loader.style.display = "none";
+            
+})
+
 $(document).ready(function(){
     $('.slider').slider({
         indicators: false,
-        height: 350,
+        height: 450,
         duration: 500
     });
+
+    $('.pushpin').pushpin();
+
+    $('.target').pushpin({
+        top: 0,
+        bottom: 1000,
+        offset: 0
+    });
+
+    $('.pushpin-demo-nav').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+      });  
   });
+
